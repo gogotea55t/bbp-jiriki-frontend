95a85db8bfccef0c799c35c49444b8e65204b765
gogotea55t <chirigami05@gmail.com>
2018-12-30 01:20:50 +0900

    表示はできるところまで

---
+++ components/SongCol.vue
@@ -0,0 +1,14 @@
+<template>
+  <span>Hello.</span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: ['SongCol'],
+  data: function() {
+    return {}
+  }
+})
+</script>
--- pages/songlist.vue
+++ pages/songlist.vue
@@ -1,8 +1,46 @@
 <template>
-  <span>Hello!!</span>
+  <table class="table">
+    <tr>
+      <th>地力</th>
+      <th>楽曲名</th>
+      <th>投稿者名</th>
+      <th>楽器名</th>
+    </tr>
+    <tr
+      v-for="song in songs"
+      :key="song.id">
+      <td>{{ song.jirikiRank }}</td>
+      <td>{{ song.songName }}</td>
+      <td>{{ song.contributor }}</td>
+      <td>{{ song.instrument }}</td>
+    </tr>
+  </table>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+// import SongCol from '../components/SongCol.vue'
+
+export default Vue.extend({
+  data: function() {
+    return {
+      songs: [
+        {
+          id: '001',
+          jirikiRank: '地力S+',
+          songName: 'カミサマネジマキ',
+          contributor: 'ミラ',
+          instrument: 'ロックオルガン'
+        },
+        {
+          id: '002',
+          jirikiRank: '地力S+',
+          songName: 'インフェルノ',
+          contributor: 'コメの',
+          instrument: 'Ｄ・ギター①'
+        }
+      ]
+    }
+  }
+})
 </script>
