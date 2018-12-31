ad132519d6c833aed6490e9f6570f912e966379b
gogotea55t <chirigami05@gmail.com>
2018-12-31 17:32:17 +0900

    envの設定に成功

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
 const pkg = require('./package')
+const environment = process.env.NODE_ENV || 'development';
+const envSet = require(`./env.${environment}.js`)
 
 module.exports = {
   mode: 'spa',
@@ -28,6 +30,8 @@ module.exports = {
   */
   css: [
   ],
+
+  env: envSet,
 
   /*
   ** Plugins to load before mounting the App
