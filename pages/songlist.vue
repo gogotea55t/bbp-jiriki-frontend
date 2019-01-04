3ba3f2f680bf5f87f87192a875f41b9280ee7388
gogotea55t <chirigami05@gmail.com>
2019-01-04 18:44:05 +0900

    ローディング画像を入れる+次のを読み込む機構を入れる

--- pages/songlist.vue
+++ pages/songlist.vue
@@ -1,16 +1,21 @@
 <template>
-  <table class="table">
-    <tr>
-      <th>地力</th>
-      <th>楽曲名</th>
-      <th>投稿者名</th>
-      <th>楽器名</th>
-    </tr>
-    <SongCol
-      v-for="song in songs"
-      v-bind="song"
-      :key="song.songId"/>
-  </table>
+  <div>
+    <table class="table">
+      <tr>
+        <th>地力</th>
+        <th>楽曲名</th>
+        <th>投稿者名</th>
+        <th>楽器名</th>
+      </tr>
+      <SongCol
+        v-for="song in songs"
+        v-bind="song"
+        :key="song.songId"/>
+    </table>
+    <img
+      src="~/static/loading.gif"
+      alt="now loading...">
+  </div>
 </template>
 
 <script lang="ts">
@@ -23,13 +28,39 @@ export default Vue.extend({
   data: function() {
     console.log(process.env.apiBaseUrl)
     return {
-      songs: []
+      songs: [],
+      page: 1
+    }
+  },
+  mounted: function() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    getMore: function() {
+      console.log('呼ばれてはいる')
+      let more = axios
+        .get(process.env.apiBaseUrl + '/songs?page=' + this.page)
+        .then(response => {
+          if (response.data.length > 0) {
+            for (let d of response.data) {
+              this.songs.push(d)
+            }
+            this.page += 1
+          } else {
+            console.log(response.data)
+          }
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    handleScroll() {
+      console.log(this)
     }
   },
   asyncData(context) {
     let url = process.env.apiBaseUrl + '/songs/'
     let songs = []
-    console.log(url)
     return axios
       .get(url)
       .then(response => {
