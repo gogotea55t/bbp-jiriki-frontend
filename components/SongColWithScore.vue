fdb37f9c50d1d7af8f8a2299d9fc5ba15177cd38
gogotea55t <chirigami05@gmail.com>
2019-04-21 00:03:04 +0900

    fixed #36

---
+++ components/SongColWithScore.vue
@@ -0,0 +1,26 @@
+<template>
+  <tr>
+    <JirikiRank :jiriki-rank="song.jirikiRank"></JirikiRank>
+    <td>{{ song.songName }}</td>
+    <td>{{ song.contributor }}</td>
+    <td>{{ song.instrument }}</td>
+    <ScoreStyle :score="song.score"></ScoreStyle>
+  </tr>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SongsWithScore from './SongsWithScore'
+import JirikiRank from './JirikiRank.vue'
+import ScoreStyle from './ScoreStyle.vue'
+export default Vue.extend({
+  name: 'SongColWithScore',
+  components: { JirikiRank, ScoreStyle },
+  props: {
+    song: {
+      type: SongsWithScore,
+      default: new SongsWithScore('', '', '', '', '', 0)
+    }
+  }
+})
+</script>
