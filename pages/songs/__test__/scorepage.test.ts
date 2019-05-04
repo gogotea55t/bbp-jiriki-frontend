09d02fe5eec15ec9ecb6188237d948d9604a78ff
gogotea55t <chirigami05@gmail.com>
2019-05-04 11:48:52 +0900

    test: スコア詳細画面のテストを追加

--- pages/songs/__test__/scorepage.test.ts
+++ pages/songs/__test__/scorepage.test.ts
@@ -1,18 +1,50 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import * as ScorePage from '../_id.vue'
+import MockAdapter from 'axios-mock-adapter'
+import axios from 'axios'
 
+jest.setTimeout(20000)
+const mock = new MockAdapter(axios)
+const apiBaseUrl: any = process.env.apiBaseUrl //|| 'http://localhost:8080'
+
+const sampleSong = {
+  songId: '200',
+  jirikiRank: '地力Ａ＋',
+  songName: 'ミラクルペイント',
+  contributor: 'タタナミ',
+  instrument: 'ピアノ①'
+}
+
+const sampleScores = [
+  { userName: '妖怪1', score: 100 },
+  { userName: '妖怪2', score: 99 },
+  { userName: 'どちらかというと妖怪', score: 88 },
+  { userName: 'どちらかというと人間', score: 79 },
+  { userName: '人間', score: 64 }
+]
+
+mock.onGet(apiBaseUrl + '/songs/200').reply(200, sampleSong)
+mock.onGet(apiBaseUrl + '/songs/200/scores').reply(200, sampleScores)
 describe(ScorePage.default, () => {
-  const wrapper = shallowMount(ScorePage.default, {
-    mocks: {
-      $route: {
-        params: {
-          id: '200'
-        }
+  it('サーバーからデータを取得できる', done => {
+    const wrapper = mount(ScorePage.default, {
+      mocks: {
+        $route: {
+          params: {
+            id: '200'
+          }
+        },
+        Promise,
+        mock
       }
-    }
-  })
+    })
 
-  it('表示できる', () => {
     expect(wrapper.vm.$data.id).toBe('200')
+    setTimeout(done2 => {
+      expect(wrapper.vm.$data.scores.length).toBe(5)
+      expect(wrapper.vm.$data.song.songId).toBe('200')
+      done2
+      done()
+    }, 5000)
   })
 })
