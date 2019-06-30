f5b2789ce42b176025387e7d26b0de5a78f427d5
gogotea55t <chirigami05@gmail.com>
2019-06-30 23:05:58 +0900

    feat: 楽曲情報を別コンポーネントに分割

---
+++ components/SongInfo.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>地力</th>
+          <th>楽曲名</th>
+          <th>投稿者名</th>
+          <th>楽器名</th>
+        </tr>
+      </thead>
+      <tbody>
+        <SongCol :song="song" />
+      </tbody>
+    </table>
+    <table class="table">
+      <tr>
+        <th>プレイヤー</th>
+        <th>得点</th>
+      </tr>
+      <tr v-for="score in scores" :key="score.id">
+        <td>{{ score.userName }}</td>
+        <ScoreStyle :score="score.score" />
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SongCol from './SongCol.vue'
+import ScoreStyle from './ScoreStyle.vue'
+import Songs from './Songs'
+import axios from 'axios'
+export default Vue.extend({
+  components: { SongCol, ScoreStyle },
+  props: {
+    songId: {
+      type: String,
+      default: ''
+    }
+  },
+  data: function() {
+    return {
+      song: new Songs(this.songId, '', '', '', ''),
+      scores: []
+    }
+  },
+  computed: {
+    songsIdentifier: function() {
+      return (
+        this.$data.song.songName +
+        ' / ' +
+        this.$data.song.contributor +
+        ' (' +
+        this.$data.song.instrument +
+        ')'
+      )
+    }
+  },
+  watch: {
+    songId: function() {
+      this.song = new Songs('', '', '', '', '')
+      this.scores = []
+      this.loadInfo(this.songId)
+    }
+  },
+  async created() {
+    if (this.songId === '') {
+      return
+    } else {
+      this.loadInfo(this.songId)
+    }
+  },
+  methods: {
+    async loadInfo(id) {
+      let songResponse: Songs = await axios
+        .get(process.env.apiBaseUrl + '/v1' + '/songs/' + id)
+        .then(response => {
+          let s: any = response.data
+          return new Songs(
+            s.songId,
+            s.jirikiRank,
+            s.songName,
+            s.contributor,
+            s.instrument
+          )
+        })
+        .catch(error => {
+          console.log(error)
+          throw new Error('サーバーとの接続に失敗しました')
+        })
+
+      let scoreResponse = await axios
+        .get(process.env.apiBaseUrl + '/v1' + '/songs/' + id + '/scores')
+        .then(response => {
+          return response.data
+        })
+        .catch(error => {
+          console.log(error)
+          throw new Error('サーバーとの接続に失敗しました')
+        })
+      this.$data.song = songResponse
+      this.$data.scores = scoreResponse
+      this.$emit(
+        'song-loaded',
+        this.$data.song.songName +
+          ' / ' +
+          this.$data.song.contributor +
+          ' (' +
+          this.$data.song.instrument +
+          ')'
+      )
+    }
+  }
+})
+</script>
