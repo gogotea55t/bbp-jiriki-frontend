80148bcd0aa400a1a4758977dc5bc421c2565824
gogotea55t <chirigami05@gmail.com>
2019-08-17 15:54:11 +0900

    test: handleLoginEventを理解

--- pages/__test__/callback.test.ts
+++ pages/__test__/callback.test.ts
@@ -2,6 +2,10 @@ import { shallowMount } from '@vue/test-utils'
 import * as Callback from '../callback.vue'
 import { AuthServiceStub } from '../__stubs__/AuthServiceMock'
 
+const mockRouter = {
+  push: jest.fn()
+}
+
 describe(Callback.default, () => {
   it('表示できる', () => {
     const wrapper = shallowMount(Callback.default, {
@@ -10,5 +14,21 @@ describe(Callback.default, () => {
       }
     })
     expect(wrapper.isVueInstance).toBeTruthy
+  })
+
+  it('ログインイベント発生', () => {
+    const wrapper = shallowMount(Callback.default, {
+      mocks: {
+        $auth: new AuthServiceStub(),
+        $router: mockRouter
+      }
+    })
+
+    const vueInstance: any = wrapper.vm
+    vueInstance.handleLoginEvent({
+      loggedIn: true,
+      profile: {},
+      state: {}
+    })
   })
 })
