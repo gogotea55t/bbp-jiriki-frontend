0b11f015523181aec27b0dbcf251ff7df2150a43
gogotea55t <chirigami05@gmail.com>
2020-04-12 23:59:36 +0900

    test: feat: 空欄でエンターキーを押すと削除するテストを追加

--- components/atoms/__test__/ScoreForm.test.ts
+++ components/atoms/__test__/ScoreForm.test.ts
@@ -3,6 +3,7 @@ import * as ScoreForm from '../ScoreForm.vue'
 import MockAdapter from 'axios-mock-adapter'
 import axios from 'axios'
 import Vue from 'vue'
+import { ok } from 'assert'
 
 const mock = new MockAdapter(axios)
 mock
@@ -20,6 +21,24 @@ mock
     score: '92'
   })
   .reply(500, {})
+
+mock
+  .onDelete(process.env.apiBaseUrl + '/v1/scores', {
+    data: {
+      userId: 'u001',
+      songId: '334'
+    }
+  })
+  .reply(204, {})
+
+mock
+  .onDelete(process.env.apiBaseUrl + '/v1/scores', {
+    data: {
+      userId: 'u004',
+      songId: '334'
+    }
+  })
+  .reply(404, {})
 
 // このおまじないがないとv-Tooltipでエラーが出る
 global.document.createRange = (): any => ({
@@ -82,6 +101,46 @@ describe(ScoreForm.default, () => {
         expect(event[0][0]).toBe('90')
       } else {
         fail()
+      }
+    })
+  })
+
+  it('空欄にしたときにエンターキーを押すとスコア削除ができる', () => {
+    wrapper.find('input').trigger('focus')
+    wrapper.vm.$data.scoreForm = ''
+    wrapper.find('input').trigger('keyup.enter')
+    Vue.nextTick(() => {
+      expect(wrapper.vm.$data.submitMsg).toBe('登録を削除しました')
+      const event = wrapper.emitted('score-submitted')
+      if (event) {
+        expect(event[0][0]).toBe('')
+      } else {
+        fail()
+      }
+    })
+  })
+
+  it('空欄にしたときにエンターキーを押してスコア削除に失敗するとその旨表示される', () => {
+    const wrapper2 = shallowMount(ScoreForm.default, {
+      propsData: {
+        score: 88,
+        songId: '334',
+        userId: 'u004'
+      },
+      mocks: {
+        mock
+      }
+    })
+    wrapper2.find('input').trigger('focus')
+    wrapper2.vm.$data.scoreForm = ''
+    wrapper2.find('input').trigger('keyup.enter')
+    Vue.nextTick(() => {
+      expect(wrapper2.vm.$data.submitMsg).toBe('削除失敗です')
+      const event = wrapper2.emitted('score-submitted')
+      if (event) {
+        fail()
+      } else {
+        ok
       }
     })
   })
