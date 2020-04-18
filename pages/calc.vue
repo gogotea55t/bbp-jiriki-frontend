892086af91d17f0ac565fdac9f0dd395b216f88d
gogotea55t <chirigami05@gmail.com>
2020-04-18 21:04:14 +0900

    feat: tail_missなしで出来るところまで

--- pages/calc.vue
+++ pages/calc.vue
@@ -1,23 +1,74 @@
 <template>
   <div>
-    <div class="field is-horizontal">
-      <label class="label">BEST</label
-      ><input v-model.number="best" class="input" type="number" />
+    <h2>スコア計算機</h2>
+    <div class="field is-horizontal control">
+      <label class="field-label best">BEST</label>
+      <input
+        v-model.number="best"
+        class="field-body input"
+        type="number"
+        max="10000"
+        min="0"
+      />
     </div>
     <div class="field is-horizontal">
-      <label class="label">GOOD</label
-      ><input v-model.number="good" class="input" type="number" />
+      <label class="field-label good">GOOD</label
+      ><input
+        v-model.number="good"
+        class="field-body input"
+        type="number"
+        max="10000"
+        min="0"
+      />
     </div>
     <div class="field is-horizontal">
-      <label class="label">BAD</label
-      ><input v-model.number="bad" class="input" type="number" />
+      <label class="field-label bad">BAD</label
+      ><input
+        v-model.number="bad"
+        class="field-body input"
+        type="number"
+        max="10000"
+        min="0"
+      />
     </div>
     <div class="field is-horizontal">
-      <label class="label">MISS</label
-      ><input v-model.number="miss" class="input" type="number" />
+      <label class="field-label miss">MISS</label
+      ><input
+        v-model.number="miss"
+        class="field-body input"
+        type="number"
+        max="10000"
+        min="0"
+      />
     </div>
-    <div>TOTAL NOTES: {{ total }}</div>
-    <div>SCORE: {{ scoreView }}</div>
+    <div class="field is-horizontal">
+      <label class="field-label total">TOTAL NOTES</label
+      ><input
+        v-model.number="total"
+        class="field-body input"
+        type="number"
+        readonly
+      />
+    </div>
+    <article v-if="isValid">
+      得点は <span class="numeral">{{ scoreView }}</span
+      >.<span class="decimal">{{ decimal }}</span
+      >点です。
+    </article>
+    <article v-else>
+      負の数を入力するのはやめてください＞＜
+    </article>
+    <article v-if="isValid">
+      <div v-if="lossByGood > 0">
+        GOODでの失点は{{ lossByGood.toFixed(2) }}点です。
+      </div>
+      <div v-if="lossByBad > 0">
+        BADでの失点は{{ lossByBad.toFixed(2) }}点です。
+      </div>
+      <div v-if="lossByMiss > 0">
+        MISSでの失点は{{ lossByMiss.toFixed(2) }}点です。
+      </div>
+    </article>
   </div>
 </template>
 
@@ -26,7 +77,7 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
-      best: 0,
+      best: 1,
       good: 0,
       bad: 0,
       miss: 0
@@ -34,7 +85,12 @@ export default Vue.extend({
   },
   computed: {
     total(): Number {
-      return this.best + this.good + this.bad + this.miss
+      return (
+        Number(this.best) +
+        Number(this.good) +
+        Number(this.bad) +
+        Number(this.miss)
+      )
     },
     score(): Number {
       if (this.total) {
@@ -48,11 +104,67 @@ export default Vue.extend({
       }
     },
     isValid(): boolean {
-      return false
+      return !(this.best < 0 || this.good < 0 || this.bad < 0 || this.miss < 0)
     },
     scoreView(): Number {
       return Math.floor(this.score.valueOf())
+    },
+    decimal(): String {
+      return (
+        Math.floor((this.score.valueOf() - this.scoreView.valueOf()) * 100) /
+        100
+      )
+        .toFixed(2)
+        .slice(-2)
+    },
+    lossByGood(): number {
+      return ((this.good * 0.15) / this.total.valueOf()) * 100
+    },
+    lossByBad(): number {
+      return ((this.bad * 0.35) / this.total.valueOf()) * 100
+    },
+    lossByMiss(): number {
+      return (this.miss / this.total.valueOf()) * 100
     }
   }
 })
 </script>
+
+<style>
+.best {
+  background-color: red;
+  color: white;
+  text-align: center;
+  font-size: 150%;
+}
+
+.good {
+  background-color: orange;
+  text-align: center;
+  font-size: 150%;
+}
+
+.bad {
+  background-color: #1133ee;
+  color: white;
+  text-align: center;
+  font-size: 150%;
+}
+
+.miss {
+  background-color: gray;
+  color: white;
+  text-align: center;
+  font-size: 150%;
+}
+
+.total {
+  background-color: greenyellow;
+  text-align: center;
+  font-size: 110%;
+}
+
+.numeral {
+  font-size: 25px;
+}
+</style>
