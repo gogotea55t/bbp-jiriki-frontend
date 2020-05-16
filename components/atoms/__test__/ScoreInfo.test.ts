99cf021ae8d6bceb4ccbf38d5cdfdba91a191c96
gogotea55t <chirigami05@gmail.com>
2020-05-16 22:49:13 +0900

    test: feat: 一部テストが出来てなかったので修正

--- components/atoms/__test__/ScoreInfo.test.ts
+++ components/atoms/__test__/ScoreInfo.test.ts
@@ -1,5 +1,18 @@
 import { mount } from '@vue/test-utils'
 import * as ScoreInfo from '../ScoreInfo.vue'
+import MockAdapter from 'axios-mock-adapter'
+import axios from 'axios'
+
+const mock = new MockAdapter(axios)
+
+mock.onGet(process.env.apiBaseUrl + '/v2/songs/33/top').reply(200, {
+  top: [
+    { userId: 'u004', userName: '妖怪1', score: 100 },
+    { userId: 'u005', userName: '妖怪Ｂ', score: 100 }
+  ],
+  second: [{ userId: 'u002', userName: '人間', score: 93 }],
+  third: []
+})
 
 describe(ScoreInfo.default, () => {
   it('renders Score detail style.', () => {
@@ -7,8 +20,11 @@ describe(ScoreInfo.default, () => {
     expect(wrapper.isVueInstance).toBeTruthy
   })
 
-  it('tooltipのオンオフを切り替えると切り替わる', () => {
+  it('tooltipのオンオフを切り替えると切り替わる', done => {
     const wrapper = mount(ScoreInfo.default, {
+      mocks: {
+        mock
+      },
       propsData: {
         songId: '33',
         score: 88,
@@ -19,8 +35,15 @@ describe(ScoreInfo.default, () => {
 
     const vueInstance: any = wrapper.vm
     vueInstance.changeTooltip(true)
-    expect(wrapper.vm.$data.isTooltipShow).toBeTruthy
-    vueInstance.changeTooltip(false)
-    expect(wrapper.vm.$data.isTooltipShow).toBeFalsy
+    setTimeout(done2 => {
+      expect(wrapper.vm.$data.isTooltipShow).toBeTruthy
+      vueInstance.changeTooltip(false)
+      setTimeout(done3 => {
+        expect(wrapper.vm.$data.isTooltipShow).toBeFalsy
+        done()
+        done3
+      }, 500)
+      done2
+    }, 500)
   })
 })
